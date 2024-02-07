45c2fbe64e0fd5e68e3100cfe9eddeb73278c905
whyalwaysnuew <coworasavanila@gmail.com>
2024-02-07 15:42:06 +0700

    Feat: param id to slug

--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -25,7 +25,7 @@ class TypeController extends Controller
             return DataTables::of($query)
                     ->addColumn('action', function($type){
                         return '<div class="flex justify-between gap-2">
-                            <a href="' . route('admin.types.edit', $type->id) . '" class="text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 rounded-lg text-sm p-2.5 text-center inline-flex items-center dark:bg-yellow-400 dark:hover:bg-yellow-400 dark:focus:ring-yellow-500" title="Edit">
+                            <a href="' . route('admin.types.edit', $type->slug) . '" class="text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 rounded-lg text-sm p-2.5 text-center inline-flex items-center dark:bg-yellow-400 dark:hover:bg-yellow-400 dark:focus:ring-yellow-500" title="Edit">
                                 <svg class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                                     <path fill-rule="evenodd" d="M11.3 6.2H5a2 2 0 0 0-2 2V19a2 2 0 0 0 2 2h11c1.1 0 2-1 2-2.1V11l-4 4.2c-.3.3-.7.6-1.2.7l-2.7.6c-1.7.3-3.3-1.3-3-3.1l.6-2.9c.1-.5.4-1 .7-1.3l3-3.1Z" clip-rule="evenodd"/>
                                     <path fill-rule="evenodd" d="M19.8 4.3a2.1 2.1 0 0 0-1-1.1 2 2 0 0 0-2.2.4l-.6.6 2.9 3 .5-.6a2.1 2.1 0 0 0 .6-1.5c0-.2 0-.5-.2-.8Zm-2.4 4.4-2.8-3-4.8 5-.1.3-.7 3c0 .3.3.7.6.6l2.7-.6.3-.1 4.7-5Z" clip-rule="evenodd"/>
@@ -95,8 +95,10 @@ class TypeController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit(Type $type)
+    public function edit($slug)
     {
+        $type = Type::where('slug', $slug)->firstOrFail();
+
         $data = [
             'type' => $type
         ];
